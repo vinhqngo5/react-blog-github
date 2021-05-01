087e690ea9d2b763deb9d87f1cbff7991c3b8675
vinhqngo5 <vinhqngo5@gmail.com>
2021-05-01 09:56:07 +0700

    Updates

---
+++ precache-manifest.2bc572ce630caf2b731ba584274b1135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16627119ecd235aaa3ab1bb43edddd8c",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "ca0cc3a13472544a0dc5",
+    "url": "/react-blog-github/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "cf352fcb46f8dc475556",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca0cc3a13472544a0dc5",
+    "url": "/react-blog-github/static/js/main.388c13df.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  }
+]);
